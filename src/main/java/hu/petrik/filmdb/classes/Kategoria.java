c2f0442cd422b2c1e53c6e1382f389f1e6c51089
Schmidt Gergő <s.gergo.az@gmail.com>
2022-03-09 11:07:18 +0100

    Kategoria

---
+++ src/main/java/hu/petrik/filmdb/classes/Kategoria.java
@@ -0,0 +1,23 @@
+package hu.petrik.filmdb.classes;
+
+public class Kategoria {
+    private int id;
+    private String kategoria;
+
+    public Kategoria(int id, String kategoria) {
+        this.id = id;
+        this.kategoria = kategoria;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getKategoria() {
+        return kategoria;
+    }
+
+    public void setKategoria(String kategoria) {
+        this.kategoria = kategoria;
+    }
+}
